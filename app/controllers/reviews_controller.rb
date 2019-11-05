baacd20a8aaa5f2401442ca2569c5d6e997c9aa1
y-takahara <yuuki.barber@gmail.com>
2019-11-05 20:24:31 +0900

    add:devise

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,32 @@
 class ReviewsController < ApplicationController
 
-  def index
+  def home
   end
+
+  def index
+    @reviews = Review.find(15)
+
+  end
+
+  def new
+    @review = Review.new
+  end
+
+  def show
+  end
+
+  def create
+    @review = Review.new(review_params)
+    @review.save
+      render :home
+  end
+
+
+
+  private
+
+  def review_params
+    params.require(:review).permit(:event_date,:artist,:venue,:image,:text).merge(user_id: current_user.id)
+  end
+
 end
