c3ca0cac2873e9e879f2fab7f7d5caadee784905
y-takahara <yuuki.barber@gmail.com>
2019-11-08 14:42:11 +0900

    マイページのユーザー画像表示の実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,12 @@ class UsersController < ApplicationController
   def show 
     user = User.find(params[:id])
     @nickname = user.nickname
+    @avatar = 
+      if user.avatar.attached?
+        user.avatar
+      else
+        nil
+      end
     @reviews = user.reviews.order("id DESC").page(params[:page]).per(6)
   end
 end
