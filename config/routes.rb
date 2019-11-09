b6e088579a7655dea67007fa1ad296906b331127
y-takahara <yuuki.barber@gmail.com>
2019-11-09 10:13:57 +0900

    fix: comment destroy

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   get "reviews/search" => "reviews#search"
   resources :reviews do
     resources :likes, only: [:create, :destroy]
-    resources :comments, only:[:create]
+    resources :comments, only:[:create,:destroy]
   end
   resources :users, only:[:show]
   
