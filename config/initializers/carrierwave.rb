ede584fc41dd3da5e264f92d483f47b0837e5f4e
y-takahara <yuuki.barber@gmail.com>
2019-11-10 19:00:18 +0900

    add: s3

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,18 @@
+require 'carrierwave/storage/abstract'
+require 'carrierwave/storage/file'
+require 'carrierwave/storage/fog'
+
+if Rails.env.production?
+  CarrierWave.configure do |config|
+    config.fog_provider = 'fog/aws'
+    config.fog_credentials = {
+      provider: 'AWS',
+      aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+      region: 'ap-northeast-1'  # S3バケット作成時に指定したリージョン。左記は東京を指す
+    }
+    config.fog_directory  = 'livevoice' # 作成したS3バケット名
+  end
+  # 日本語ファイル名の設定
+  CarrierWave::SanitizedFile.sanitize_regexp = /[^[:word:]\.\-\+]/ 
+end
